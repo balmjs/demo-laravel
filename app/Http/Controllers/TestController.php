d20a41b63e9647f7d51bab2b8ffb240dac659de0
Elf-mousE <ifmouz@gmail.com>
2016-10-21 12:44:51 +0800

    update

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,15 +11,27 @@ class TestController extends Controller
     public function index(Request $request) {
         if ($request->isMethod('post')) {
             $username = $request->username;
-            echo 'Username:'. $username.'<br>';
 
             if ($request->hasFile('photo')) {
-              $file = $request->file('photo');
-              $path = $file->move('uploads', 'test.png');
-              echo 'Yes';
+                $file = $request->file('photo');
+                $path = $file->move('uploads', 'test.png');
+                $result = [
+                    'code' => 200,
+                    'msg' => 'Yes',
+                    'data' => [
+                      'username' => $username
+                    ]
+                ];
             } else {
-              echo 'No';
+                $result = [
+                    'code' => 201,
+                    'msg' => 'No',
+                    'data' => [
+                      'username' => $username
+                    ]
+                ];
             }
+            return response()->json($result);
         }
         return view('test');
     }
