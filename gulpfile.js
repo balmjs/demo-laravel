72d816a38c4d24b16ab46e26c5be6893c16d42c2
elf-mouse <ifmouz@gmail.com>
2018-02-01 11:48:21 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ let balm = require('balm');
 balm.config = {
   static: false, // for PHP framework
   server: {
-    proxy: 'your.project.dev'
+    proxy: 'your.project.local'
   },
   roots: {
     source: 'resources',
@@ -35,10 +35,10 @@ balm.config = {
       vue: balm.config.production ? 'vue/dist/vue.min.js' : 'vue/dist/vue.js'
     }
   },
-  // cache: true,
   // assets: {
   //   subDir: 'assets'
-  // }
+  // },
+  // cache: true
 };
 
 balm.go(function(mix) {
